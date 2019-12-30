f9cabf4f38947ddd3c3ffec3d2d6649ac174f89f
ervin1718012 <fadlyerviansyah@gmail.com>
2019-12-30 22:23:28 +0700

    fix selesai

--- app/src/main/java/com/example/erviansyahf/MainActivity.java
+++ app/src/main/java/com/example/erviansyahf/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.erviansyahf;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView ivsetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        ivsetting = findViewById(R.id.icSetting);
+        ivsetting.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        Intent setting = new Intent(MainActivity.this,app_setting.class);
+        startActivity(setting);
     }
 }
+
